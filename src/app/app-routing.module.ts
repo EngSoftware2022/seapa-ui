e6b7ccd0747e496d113b218d1da35f6242902989
Matheus Lima <lima.matheus@unifesp.br>
2022-10-17 18:32:11 -0300

    Rotas Criadas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NewUserComponent } from './pages/new-user/new-user.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'new-user', component: NewUserComponent }];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [ RouterModule ],
+  imports: [ RouterModule.forRoot(routes)]
 })
-export class AppRoutingModule { }
+
+export class AppRoutingModule {}
