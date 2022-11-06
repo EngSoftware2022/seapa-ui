612275200f34a4e6c459954ddf424d52e6e33f9a
aamgoulart <aamgoulart@outlook.com>
2022-11-06 15:13:55 -0300

    table userfriends

--- src/app/pages/friend-request/friend-request.component.ts
+++ src/app/pages/friend-request/friend-request.component.ts
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FriendRequestComponent implements OnInit {
 
+
+  friendList: any[] | undefined;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.friendList = [{
+      username: "aamgoulart",
+      firstName: "Amanda",
+      lastName: "Goulart",
+      friend: 1
+    },
+    {
+      username: "aamgoulart",
+      firstName: "Amanda",
+      lastName: "Goulart",
+      friend: 0
+    }, {
+      username: "aamgoulart",
+      firstName: "Amanda",
+      lastName: "Goulart",
+      friend: 2
+    }]
   }
 
 }
